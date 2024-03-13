d8344eff99d4638bc8775bab20d7547710a56f0e
Misha McFeat <misha.mcfeat@gmail.com>
2024-03-13 12:47:03 +0000

    Adding main program structure to prompt information about user

--- backend/AcmeBank.Presentation/AcmeBank.ConsoleApp/Program.cs
+++ backend/AcmeBank.Presentation/AcmeBank.ConsoleApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using AcmeBank.Application.Services;
+
+namespace AcmeBank.ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Acme Bank");
+        Console.WriteLine("Please enter your account number");
+        var accountNumber = Console.ReadLine();
+        Console.WriteLine("Please enter your sort code");
+        var sortCode = Console.ReadLine();
+        
+        // Call the account service to get the account
+        var accountService = new AccountService();
+        //
+        var account = accountService.GetAccountByNumber(accountNumber, sortCode);
+    }
+}

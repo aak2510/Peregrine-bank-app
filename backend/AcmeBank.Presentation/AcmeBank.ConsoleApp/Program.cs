6f905363ac73942680c37915a2524f51a1d87dec
aak2510 <akanda2500@gmail.com>
2024-03-15 12:26:23 +0000

    updated program.cs

--- backend/AcmeBank.Presentation/AcmeBank.ConsoleApp/Program.cs
+++ backend/AcmeBank.Presentation/AcmeBank.ConsoleApp/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using AcmeBank.Application.Services;
-
-namespace AcmeBank.ConsoleApp;
-
+﻿
 class Program
 {
     static void Main(string[] args)
@@ -12,38 +8,51 @@ class Program
         //var accountNumber = Console.ReadLine();
         //Console.WriteLine("Please enter your sort code");
         //var sortCode = Console.ReadLine();
-        
+
         //// Call the account service to get the account
         //var accountService = new AccountService();
         ////
         //var account = accountService.GetAccountByNumber(accountNumber, sortCode);
 
 
-        // Check Tellers credentials are correct, if they are then proceed, else repeat until they enter the correct username and password.
+        //store username as lowercase
+        string name = "teller".ToLower();
+        string password = "password";
+        int logInCount = 0;
+        // Check that log-in credentials are correct, if they are then proceed, else repeat until they enter the correct username and password.
         bool inputSuccess = false;
 
-
+        
         do
         {
-            // Ask Teller for username, not case sensitive, so we convert to the same case
+            // Ask user for username, not case sensitive, so we convert to the same case
             Console.Write("Please enter your username: ");
-            string? inputUsername = Console.ReadLine().ToLower().Trim();
+            string inputUsername = Console.ReadLine().ToLower().Trim();
             // Password IS case sensitive, so we don't change case
             Console.Write("Please enter your password: ");
-            string? inputPassword = Console.ReadLine().Trim();
+            string inputPassword = Console.ReadLine().Trim();
 
             // Only leave the log in page if BOTH username and password are the same
-            if (inputUsername == primaryUser.name && inputPassword == primaryUser.password)
+            if (inputUsername == name && inputPassword == password || !string.IsNullOrEmpty(inputUsername) || !string.IsNullOrEmpty(inputUsername))
             {
                 inputSuccess = true;
                 Console.WriteLine();
             }
             else
             {
+                logInCount += 1;
                 // Message to let user know that input is incorrect
                 Console.WriteLine("Username or password is incorrect, please try again.\n");
+      
+            }
+
+            if(logInCount == 3)
+            {
+                Console.WriteLine("Too many attempts");
+                inputSuccess = true;
             }
         } while (!inputSuccess);
+
 
     }
 }

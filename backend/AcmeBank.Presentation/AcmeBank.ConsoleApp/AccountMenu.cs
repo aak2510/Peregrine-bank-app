46200cc8ef58de9834f07561a42affd63834ad9f
aak2510 <akanda2500@gmail.com>
2024-03-18 16:26:47 +0000

    updated

--- backend/AcmeBank.Presentation/AcmeBank.ConsoleApp/AccountMenu.cs
+++ backend/AcmeBank.Presentation/AcmeBank.ConsoleApp/AccountMenu.cs
@@ -1,49 +1,49 @@
 namespace DefaultNamespace;
 
 // // Account Menu Class Handles interactions that the teller has while operating on an account. Maybe a separate Menu Superclass should exist?
- protected class AccountMenu
- {
-     // Stores the customer object to easily make calls to the database
-     private Customer _customer = new Customer();
-     
-     // Stores the accounts information
-     private List<Account> _accounts = new List<Account>();
-     
-     // AccountAppService should be injected. for now it is saved here
-     private AccountService _accountService = new AccountService(IAccountRepository accountRepository);
-     
-     // Default constructor
+protected class AccountMenu
+{
+    // Stores the customer object to easily make calls to the database
+    private Customer _customer = new Customer();
+
+    // Stores the accounts information
+    private List<Account> _accounts = new List<Account>();
+
+    // AccountAppService should be injected. for now it is saved here
+    private AccountService _accountService = new AccountService(IAccountRepository accountRepository);
+
+    // Default constructor
     public AccountMenu(Customer customer)
-     {
-         this._customer = customer;
-     }
-     
-     // Display Accounts menu
-     public string ReturnAccountsMenu()
-     {
-        //have we got accounts yet?
-         this.GetUserAccounts();
-         if (this._accounts)
-         {
-             //Loop through accounts and add them to a Stringbuilder which returns the string
-         }
-         else
-        {
-             return false;
-         }
-         
-     }
-     
-     // Display Single Account menu
-     public string ReturnAccountByUserSelection(sbyte selection)
     {
-         //if 
-         //if list it bigger that selection. assume list item exists
-         if (selection-1 < this._accounts.Count)
-         {
+        this._customer = customer;
+    }
+
+    // Display Accounts menu
+    public string ReturnAccountsMenu()
+    {
+        //have we got accounts yet?
+        this.GetUserAccounts();
+        if (this._accounts)
+        {
+            //Loop through accounts and add them to a Stringbuilder which returns the string
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    // Display Single Account menu
+    public string ReturnAccountByUserSelection(sbyte selection)
+    {
+        //if 
+        //if list it bigger that selection. assume list item exists
+        if (selection - 1 < this._accounts.Count)
+        {
             //
-         }
-     }
+        }
+    }
 
     protected void GetUserAccounts()
     {
